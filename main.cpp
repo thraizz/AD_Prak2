54a32d48599be2d9a62aa46edca24284326cfd84
[Aron Schueler] <aron.schueler@dass-it.de>
2018-12-14 21:16:28 +0100

    Add 'loesche'-Method and minor debugging

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ class EVKD{
             return false;
         }
     }
-    bool operator> ( EVKD E1) {
+    bool operator> ( EVKD &E1) {
         int strVal;
         strVal = strcmp(this->Daten, E1.getDaten());
         if(strVal<0){
@@ -57,12 +57,9 @@ class TEXT{
         EVKD uebergabe(In);
         if(this->Start!=NULL){
             EVKD* temp(this->Start);
-            for(int i=0;i<Anz;){
-                if((*temp).getNext()==NULL){(*temp).setNext(uebergabe);}
-                else{
-                    (*temp).getNext();
-                    temp->getNext();}
-                i++;
+            for(int i=0;i<Anz;i++){
+                if(temp->getNext()==NULL){temp->setNext(uebergabe);}
+                else{temp->getNext();}
             }
         }
         else{
@@ -84,30 +81,56 @@ class TEXT{
             Start=&(*In);
             return;
         }
-        EVKD vergleich((*Start));
+        EVKD * vergleich(Start);
         for(int i=0; i<Anz; i++){
             // Wenn das nächste Objekt mit dem verglichen werden soll nicht existiert
             // sind wir am Ende der Liste angelagt und können einfügen.
-            if(vergleich.getNext == NULL){
-                vergleich.setNext((*In));
+            if(vergleich->getNext() == NULL){
+                vergleich->setNext((*In));
             }
             
             // Wenn In zwischen dem aktuellen und dem nächste liegt, füge dort ein.
-            else if ((*In)>vergleich && vergleich.getNext>In){
-                EVKD temp = EVKD(In->getDaten, vergleich.getNext);
-                vergleich.setNext((*In));
+            else if ((*In)>(*vergleich) && vergleich->getNext()>In){
+                EVKD temp = EVKD(In->getDaten(), vergleich->getNext());
+                vergleich->setNext((*In));
             }
 
             // Kein treffer, wiederhole mit nächstem Objekt
             else{
-                vergleich = vergleich.getNext;
+                vergleich = vergleich->getNext();
             }
             
         }
     }
 
     void loesche(int Pos){
-        
+        if(Pos>this->Anz){
+            printf("Element an Position %d konnte nicht gelöscht werden, da es außerhalb der Elemente liegt.",Pos);
+            return;
+        }
+        EVKD *preobj(this->Start);
+        if(preobj==NULL){
+            printf("Element an Position %d konnte nicht gelöscht werden, da es keine Elemente gibt.",Pos);
+            return;
+        }
+        for(int i=0; i<Pos-1;i++){
+            preobj=(preobj->getNext());
+        }
+        EVKD *posobj(preobj->getNext());
+        EVKD *nextobj(posobj->getNext());
+        if(posobj!=NULL){
+            if(nextobj!=NULL){
+               preobj->setNext(*nextobj);
+               delete posobj;
+            }
+            else{
+                // (*preobj).setNext(nullptr);
+                delete posobj;
+            }
+        }
+        else{
+            printf("Das Element exisitert nicht.");
+        }
     }
 };
 
